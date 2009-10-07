0c2491a3316d6b03a7a4277bd31b3e2e58c501b7
danielm777@gmail.com <danielm777@gmail.com@a886d41a-b2a9-11de-b8dd-ab4b6ce6d521>
2009-10-07 19:11:29 +0000

    Initial import.

---
+++ AgentSNMP/src/com/ixora/rms/agents/snmp/v2c/ConfigurationV2c.java
@@ -0,0 +1,24 @@
+/*
+ * Created on 20-Aug-2005
+ */
+package com.ixora.rms.agents.snmp.v2c;
+
+import com.ixora.rms.agents.snmp.Configuration;
+import com.ixora.rms.agents.snmp.messages.Msg;
+
+/**
+ * Configuration
+ */
+public class ConfigurationV2c extends Configuration {
+	public static final String COMUNITY = Msg.COMUNITY;
+
+	/**
+	 * Empty constructor. Required to allow the associated panel
+	 * to create an empty configuration instance for editing.
+	 */
+	public ConfigurationV2c() {
+		super();
+		setProperty(COMUNITY, TYPE_STRING, true);
+		setString(COMUNITY, "public");
+	}
+}
