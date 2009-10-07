8a6684138c9db118e7f7b98cee65b4dfaabf28f7
danielm777@gmail.com <danielm777@gmail.com@a886d41a-b2a9-11de-b8dd-ab4b6ce6d521>
2009-10-07 18:59:03 +0000

    Initial import.

---
+++ trunk/AgentHostAvailability/src/com/ixora/rms/agents/hostavailability/Configuration.java
@@ -0,0 +1,28 @@
+/*
+ * Created on Jan 25, 2004
+ */
+package com.ixora.rms.agents.hostavailability;
+
+import com.ixora.rms.agents.AgentCustomConfiguration;
+import com.ixora.rms.agents.hostavailability.messages.Msg;
+
+/**
+ * Configuration for host availability agent.
+ * @author Daniel Moraru
+ */
+public final class Configuration extends AgentCustomConfiguration {
+	public static final String PING_TIMEOUT = Msg.PING_TIMEOUT;
+	public static final String PACKET_SIZE = Msg.PACKET_SIZE;
+
+	/**
+	 * Empty constructor. Required to allow the associated panel
+	 * to create an empty configuration instance for editing.
+	 */
+	public Configuration() {
+		super();
+		setProperty(PING_TIMEOUT, TYPE_INTEGER, true);
+		setProperty(PACKET_SIZE, TYPE_INTEGER, true);
+		setInt(PING_TIMEOUT, 1000);
+		setInt(PACKET_SIZE, 32);
+	}
+}
