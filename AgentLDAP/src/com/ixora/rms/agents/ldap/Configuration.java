f35370c4c5da478f850387b09d0f0d49714254fb
danielm777@gmail.com <danielm777@gmail.com@a886d41a-b2a9-11de-b8dd-ab4b6ce6d521>
2009-10-07 19:04:19 +0000

    Initial import.

---
+++ AgentLDAP/src/com/ixora/rms/agents/ldap/Configuration.java
@@ -0,0 +1,35 @@
+/*
+ * Created on Aug 20, 2005
+ */
+package com.ixora.rms.agents.ldap;
+
+import com.ixora.rms.agents.ldap.messages.Msg;
+import com.ixora.rms.agents.utils.AuthenticationAndPortConfiguration;
+
+/**
+ * Configuration
+ * @author Daniel Moraru
+ */
+public class Configuration extends AuthenticationAndPortConfiguration {
+	public static final String BASE_DN = Msg.LDAP_CONFIG_BASE_DN;
+	public static final String TIMEOUT = Msg.LDAP_CONFIG_TIMEOUT;
+	public static final String FILTER = Msg.LDAP_CONFIG_FILTER;
+
+	/**
+	 * Empty constructor. Required to allow the associated panel
+	 * to create an empty configuration instance for editing.
+	 */
+	public Configuration() {
+		super();
+
+		setProperty(BASE_DN, TYPE_STRING, true, true);
+		setProperty(FILTER, TYPE_STRING, true, false);
+		setProperty(TIMEOUT, TYPE_INTEGER, true, false);
+
+		// Default LDAP port, the SSL version is 636
+		setInt(PORT, 389);
+		setInt(TIMEOUT, 10000);
+		setString(FILTER, "(objectclass=*)");
+		//setString(BASE_DN, "cn=monitor");
+	}
+}
---
+++ AgentLDAP/src/com/ixora/rms/agents/ldap/ConfigurationPanel.java
@@ -0,0 +1,30 @@
+/*
+ * Created on 20-Aug-2005
+ */
+package com.ixora.rms.agents.ldap;
+
+import com.ixora.rms.agents.AgentCustomConfiguration;
+import com.ixora.rms.agents.ui.AgentCustomConfigurationPanelContext;
+import com.ixora.rms.agents.ui.DefaultAgentCustomConfigurationPanel;
+
+/**
+ * ConfigurationPanel
+ * @author Daniel Moraru
+ */
+public class ConfigurationPanel extends DefaultAgentCustomConfigurationPanel {
+	/**
+	 * Constructor.
+	 * @param agentId
+	 * @param ctxt
+	 */
+	public ConfigurationPanel(String agentId, AgentCustomConfigurationPanelContext ctxt) {
+		super(agentId, ctxt);
+	}
+
+	/**
+	 * @see com.ixora.rms.ui.AgentCustomConfigurationPanel#createAgentCustomConfiguration()
+	 */
+	public AgentCustomConfiguration createAgentCustomConfiguration() {
+		return new Configuration();
+	}
+}
