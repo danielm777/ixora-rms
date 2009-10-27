b16ced85e4eded95109c5e823de6d8e4246dad7c
danielm777@gmail.com <danielm777@gmail.com@a886d41a-b2a9-11de-b8dd-ab4b6ce6d521>
2009-10-27 22:34:18 +0000

    Fixed Issue 9.

--- trunk/RMS/src/com/ixora/rms/ui/tools/agentinstaller/AgentInstallationTypeSelectorDialog.java
+++ trunk/RMS/src/com/ixora/rms/ui/tools/agentinstaller/AgentInstallationTypeSelectorDialog.java
@@ -66,7 +66,7 @@ public final class AgentInstallationTypeSelectorDialog extends AppDialog {
 		setTitle(MessageRepository.get(
                 AgentInstallerComponent.NAME,
 				Msg.AGENTINSTALLER_TITLE_SELECT_INSTALLATION_TYPE));
-		setPreferredSize(new Dimension(300, 120));
+		setPreferredSize(new Dimension(450, 160));
         fPanel = new JPanel(new BorderLayout());
 		FormPanel formPanel = new FormPanel(FormPanel.VERTICAL2);
         fPanel.add(formPanel, BorderLayout.NORTH);
