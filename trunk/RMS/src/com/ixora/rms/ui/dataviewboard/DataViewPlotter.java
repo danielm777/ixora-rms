2d41a0ad54630c499ec4951a72bc8e91c517dbec
danielm777@gmail.com <danielm777@gmail.com@a886d41a-b2a9-11de-b8dd-ab4b6ce6d521>
2009-10-31 00:24:49 +0000

    Fixed Issue 7.

---
+++ trunk/RMS/src/com/ixora/rms/ui/dataviewboard/DataViewPlotter.java
@@ -0,0 +1,27 @@
+/*
+ * Created on Nov 7, 2004
+ */
+package com.ixora.rms.ui.dataviewboard;
+
+import com.ixora.rms.ResourceId;
+import com.ixora.rms.repository.DataView;
+import com.ixora.rms.ui.EntityConfigurationPanel;
+import com.ixora.rms.ui.artefacts.dashboard.DashboardSelectorPanel;
+import com.ixora.rms.ui.artefacts.dataview.DataViewSelectorPanel;
+
+/**
+ * @author Daniel Moraru
+ */
+public interface DataViewPlotter extends
+	DataViewSelectorPanel.Callback,
+		DashboardSelectorPanel.Callback,
+			EntityConfigurationPanel.Callback {
+	/**
+	 * Plots the given view. Used for views which are not
+	 * found in the locator (i.e. created on the fly).
+	 * @param context
+	 * @param view
+	 */
+	void plot(ResourceId context, DataView view);
+
+}
