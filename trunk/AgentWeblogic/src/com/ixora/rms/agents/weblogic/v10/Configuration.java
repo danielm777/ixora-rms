c95cf61145da314abbfdbae4735e45058bae9db6
danielm777@gmail.com <danielm777@gmail.com@a886d41a-b2a9-11de-b8dd-ab4b6ce6d521>
2010-06-21 18:29:54 +0000

    1.2 work in progress - support for Weblogic 10.3.

---
+++ trunk/AgentWeblogic/src/com/ixora/rms/agents/weblogic/v10/Configuration.java
@@ -0,0 +1,30 @@
+/*
+ * Created on Jan 25, 2004
+ */
+package com.ixora.rms.agents.weblogic.v10;
+
+import com.ixora.rms.agents.utils.ConfigurationWithClasspath;
+import com.ixora.rms.agents.weblogic.messages.Msg;
+
+/**
+ * Configuration for Weblogic 10.3 agent.
+ * @author Daniel Moraru
+ */
+public class Configuration extends ConfigurationWithClasspath {
+	private static final long serialVersionUID = 8530564962055993561L;
+	public static final String JMX_CONNECTION_STRING = Msg.JMX_CONNECTION_STRING;
+
+	/**
+	 * Empty constructor. Required to allow the associated panel
+	 * to create an empty configuration instance for editing.
+	 */
+	public Configuration() {
+		super();
+        setProperty(JMX_CONNECTION_STRING, TYPE_STRING, true, true);
+		setString(USERNAME, "weblogic");
+		setString(PASSWORD, "weblogic");
+        setString(JMX_CONNECTION_STRING, "service:jmx:t3://{host}:7001/jndi/weblogic.management.mbeanservers.domainruntime");
+        setString(ROOT_FOLDER, "C:/bea/wlserver_10.3");
+		setString(CLASSPATH, "/server/lib/wlfullclient.jar");
+	}
+}
