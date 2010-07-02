3d8c22620cf9f564315bbf892ca089f26745ce67
danielm777@gmail.com <danielm777@gmail.com@a886d41a-b2a9-11de-b8dd-ab4b6ce6d521>
2010-07-02 18:32:02 +0000

    Support for SAP NetWeaver 7.1.

--- trunk/AgentSAPNetWeaver/src/com/ixora/rms/agents/sapnw/v7_1/SAPNetWeaverAgent.java
+++ trunk/AgentSAPNetWeaver/src/com/ixora/rms/agents/sapnw/v7_1/SAPNetWeaverAgent.java
@@ -1,20 +1,25 @@
-/**
- * 26-Jul-2005
- */
 package com.ixora.rms.agents.sapnw.v7_1;
 
 import java.io.File;
+import java.util.Properties;
 
+import javax.management.MBeanServerConnection;
+import javax.management.ObjectName;
 import javax.naming.Context;
 
+import com.ixora.common.utils.Utils;
 import com.ixora.rms.agents.AgentId;
 import com.ixora.rms.agents.impl.jmx.JMXAbstractAgent;
 import com.ixora.rms.agents.impl.jmx.JMXAgentExecutionContext;
+import com.ixora.rms.agents.impl.jmx.JMXConnectionMBeanServer;
 import com.ixora.rms.agents.impl.jmx.JMXEntityRoot;
+import com.ixora.rms.agents.impl.jmx.MBeanServerProxy;
 import com.ixora.rms.agents.sapnw.messages.Msg;
 import com.ixora.rms.exception.InvalidConfiguration;
+import com.sap.jmx.remote.JmxConnectionFactory;
 
 /**
+ * Sap NetWeaver J2EE monitoring agent.
  * @author Daniel Moraru
  */
 public class SAPNetWeaverAgent extends JMXAbstractAgent {
@@ -46,17 +51,86 @@ public class SAPNetWeaverAgent extends JMXAbstractAgent {
 	 * @see com.ixora.rms.agents.impl.jmx.jsr160.JMXJSR160AbstractAgent#configCustomChanged()
 	 */
 	protected void configCustomChanged() throws InvalidConfiguration, Throwable {
+		super.configCustomChanged();
 		fEnvMap.put(Context.SECURITY_PRINCIPAL, fConfiguration.getAgentCustomConfiguration().getString(Configuration.USERNAME));
 		fEnvMap.put(Context.SECURITY_CREDENTIALS, fConfiguration.getAgentCustomConfiguration().getString(Configuration.PASSWORD));
-		super.configCustomChanged();
-	}
-
-	protected String getDomainFilter() {
-		return "*";
+		fEnvMap.put(Context.PROVIDER_URL, fConfiguration.getMonitoredHost() 
+				+ ":" + fConfiguration.getAgentCustomConfiguration().getInt(Configuration.P4_PORT));
+		Properties props = new Properties();
+		props.putAll(fEnvMap);
+		MBeanServerConnection conn = JmxConnectionFactory.getMBeanServerConnection(JmxConnectionFactory.PROTOCOL_ENGINE_P4,	props);
+		fJMXConnection = new JMXConnectionMBeanServer(MBeanServerProxy.buildProxy(conn));
 	}
 	
+	/**
+	 * @see com.ixora.rms.agents.impl.jmx.JMXAbstractAgent#getKeyFilter(java.lang.String)
+	 */
 	protected String getKeyFilter(String domain) {
-		return "*";
+		if(domain.startsWith("com.sap.default")) {
+			return "*";
+			// TODO be more selective...
+			//return "name=*,j2eeType=SAP_MonitorPerNode,*";
+		} else {
+			return "*";
+		}
 	}
 
+	/**
+	 * @see com.ixora.rms.agents.impl.jmx.JMXAbstractAgent#acceptEntity(javax.management.ObjectName)
+	 */
+	protected boolean acceptEntity(ObjectName oname) {
+		String soname = oname.toString();
+		if(soname.startsWith("com.sap.default")) {
+			String j2eeType = oname.getKeyProperty("j2eeType");
+			if(Utils.isEmptyString(j2eeType) || !j2eeType.equals("SAP_MonitorPerNode")) {
+				return false;
+			}
+			return true;
+		} else {
+			return true;
+		}
+	}
+
+	/**
+	 * @see com.ixora.rms.agents.impl.jmx.JMXAbstractAgent#getEntityName(javax.management.ObjectName)
+	 */
+	protected String getEntityName(ObjectName oname) {
+		String soname = oname.toString();
+		StringBuilder buff = new StringBuilder();
+		if(soname.startsWith("com.sap.default")) {
+			String j2eeCluster = oname.getKeyProperty("SAP_J2EECluster");
+			if(!Utils.isEmptyString(j2eeCluster)) {
+				buff.append(j2eeCluster);
+			}
+			String j2eeClusterNode = oname.getKeyProperty("SAP_J2EEClusterNode");
+			if(!Utils.isEmptyString(j2eeClusterNode)) {
+				buff.append("/");
+				buff.append(j2eeClusterNode);
+			}
+			String name = oname.getKeyProperty("name");
+			if(!Utils.isEmptyString(name)) {
+				// the names start and end in double quotes, remove them
+				if(name.startsWith("\"")) {
+					name = name.substring(1);
+					if(name.endsWith("\"")) {
+						name = name.substring(0, name.length()-1);
+					}
+				}
+				buff.append(name);
+			}			
+		}
+		if(buff.length() != 0) {
+			return buff.toString();
+		}
+		return super.getEntityName(oname);
+	}
+
+	/**
+	 * @see com.ixora.rms.agents.impl.AbstractAgent#sortEntities()
+	 */
+	protected boolean sortEntities() {
+		return true;
+	}
+	
+	
 }
